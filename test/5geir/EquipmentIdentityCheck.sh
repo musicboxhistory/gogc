f00ff4c544f773e398304588bf9c3ce1cf1495bb
musicboxhistory <30741964+musicboxhistory@users.noreply.github.com>
2022-06-23 20:27:37 +0900

    commit test

---
+++ test/5geir/EquipmentIdentityCheck.sh
@@ -0,0 +1,2 @@
+curl -s localhost:8081/n5g-eir-eic/v1/equipment-status?pei=test
+curl -s localhost:8081/n5g-eir-eic/v1/equipment-status
---
+++ test/station/equipmentstatus/Get.sh
@@ -0,0 +1,1 @@
+curl -s localhost:8888/station/v1/equipmentstatus/5geir
